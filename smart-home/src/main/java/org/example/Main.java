64298d029a7640c7d63f02eb0c23080fe12700ba
StasKovalchuk1 <64376198+StasKovalchuk1@users.noreply.github.com>
2024-01-12 14:37:46 +0100

    move config to resources

--- smart-home/src/main/java/org/example/Main.java
+++ smart-home/src/main/java/org/example/Main.java
@@ -3,13 +3,23 @@ package org.example;
 import org.example.configBuilders.ConfigBuilder;
 import org.example.houses.House;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        String pathToConfig1 = "/config/config1.json";
+        String pathToConfig2 = "/config/config1.json";
+
+        URL resource = Main.class.getResource(pathToConfig1);
+        File file = new File(resource.getFile());
+        String filePath = file.getPath();
+
+        House house1 = ConfigBuilder.buildHouseFromJson(filePath);
 //        House house1 = ConfigBuilder.buildHouseFromJson("config/config1.json");
-        House house1 = ConfigBuilder.buildHouseFromJson("./config/config1.json");
-        House house2 = ConfigBuilder.buildHouseFromJson("./config/config2.json");
+//        House house1 = ConfigBuilder.buildHouseFromJson("./config/config1.json");
+//        House house2 = ConfigBuilder.buildHouseFromJson("./config/config2.json");
 
         Simulation simulation = new Simulation(house1);
         simulation.runSimulation();
